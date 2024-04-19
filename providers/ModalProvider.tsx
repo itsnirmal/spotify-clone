71635b864ae3f548d3024b5667feb19922375268
Nirmal Gautam <66936373+itsnirmal@users.noreply.github.com>
2024-04-19 16:49:53 -0500

    Providers for auth and supabase

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from "react";
+
+const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+    return (
+        <>
+            MOdals!
+        </>
+    );
+}
+
+export default ModalProvider;
